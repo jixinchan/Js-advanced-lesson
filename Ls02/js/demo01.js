da6b91083eb56cc5ca1023e3fd66314d5ec48145
jixinchan <1209980074@qq.com>
2018-03-08 12:54:17 +0800

    demos

---
+++ Ls02/js/demo01.js
@@ -0,0 +1,17 @@
+/*
+	Created by JiXinchan 2018/3/8
+*/
+//类型检测之 typeof
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof "123");
+console.log(typeof undefined);
+console.log(typeof null);        //注：此时值为object
+console.log(typeof {name:"1"});
+
+console.log(typeof Array);
+console.log(typeof Date);
+console.log(typeof Function);
+console.log(typeof Number);
+
+console.log(typeof Math);
