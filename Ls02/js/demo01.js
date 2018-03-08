2b7ffaae91c746fb24be3b084e2f17c16487de88
jixinchan <1209980074@qq.com>
2018-03-08 19:29:05 +0800

    Ls01 demo1

--- Ls02/js/demo01.js
+++ Ls02/js/demo01.js
@@ -12,6 +12,20 @@ console.log(typeof {name:"1"});
 console.log(typeof Array);
 console.log(typeof Date);
 console.log(typeof Function);
-console.log(typeof Number);
+console.log(typeof Number);      //这四个为function，因为是方法
 
 console.log(typeof Math);
+
+//类型检测之instanceof
+var a={x:23,y:45};
+console.log(a instanceof Object);//true
+var b=[1,3,45];
+console.log(b instanceof Array);//true
+console.log(b instanceof Object);//true
+
+var Person=function(){
+	name:'JiXinchan'
+};
+var p=new Person();
+console.log(p instanceof Person);//true
+console.log(p instanceof Object);//true
